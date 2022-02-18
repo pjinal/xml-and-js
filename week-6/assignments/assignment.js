4675503810163052bd4fc0e21cab39e4fc8d817c
pjinal <pjinal104@gmail.com>
2022-02-18 17:42:47 -0500

    Week6_Assignment_N01495566

--- week-6/assignments/assignment.js
+++ week-6/assignments/assignment.js
@@ -4,7 +4,7 @@ const htmlToElement = (html) => {
     template.innerHTML = html;
     return template.content.firstChild;
   };
-  
+
   //load data
   const loadData = (path, callback) => {
     const xhttp = new XMLHttpRequest();
@@ -18,40 +18,41 @@ const htmlToElement = (html) => {
   };
   
   const generateTableRow = (item) => {
-    //first way to retrieve the data
-    const rollno = item.attributes[0].textContent;
-    //another way to retrieve the data
+      //Retrieving data
+    const id = Array.from(item.getElementsByTagName(`id`))[0];
     const firstname = Array.from(item.getElementsByTagName(`firstname`))[0];
     const lastname = Array.from(item.getElementsByTagName(`lastname`))[0];
-    const nickname = Array.from(item.getElementsByTagName(`nickname`))[0];
-    const marks = Array.from(item.getElementsByTagName(`marks`))[0];
-    
-  //returning all the values in te table
+    const email = Array.from(item.getElementsByTagName(`email`))[0];
+    const gender = Array.from(item.getElementsByTagName(`gender`))[0];
+    const ip = Array.from(item.getElementsByTagName(`ip`))[0];
+  
+    //returning all the values in te table
     return `<tr>
-        <td>${rollno}</td>
-        <td>${firstname.textContent +" "+lastname.textContent}</td>
-        <td>${nickname.textContent}</td>
-        <td>${marks.textContent}</td>
-    </tr>`;
+          <td>${id.textContent}</td>
+          <td>${firstname.textContent} ${lastname.textContent}</td>
+          <td>${email.textContent}</td>
+          <td>${gender.textContent}</td>
+          <td>${ip.textContent}</td>
+      </tr>`;
   };
   
   const renderTable = (xmlData) => {
-    const table = document.getElementById("main-table");
+    const table = document.getElementById("table-main");
   
     if (!table) {
       throw new Error("No table element found");
     }
   
     const nodes = Array.from(xmlData.documentElement.childNodes).filter(
-      ({ nodeName }) => nodeName === `student`
+      ({ nodeName }) => nodeName === `person`
     );
   
-    nodes.map((studentNode) =>
-      table.appendChild(htmlToElement(generateTableRow(studentNode)))
+    nodes.map((personNode) =>
+      table.appendChild(htmlToElement(generateTableRow(personNode)))
     );
   };
   
-  loadData(`http://localhost:8080/week-6/assignments/students.xml`, renderTable);
+  loadData(`http://localhost:8080//week-6/assignments/people.xml`, renderTable);
   
   const onReset = () => {
     window.location.replace(window.location.pathname);
